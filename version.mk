1155f6097617c2a5a0654b97d46fa78ddda62ad9
Yashar Ahmedizadeh <yashade2001@gmail.com>
2014-09-14 17:30:49 +0300

    omniplusize & remove unneeded things

--- version.mk
+++ version.mk
@@ -5,15 +5,8 @@ TARGET_PRODUCT_SHORT := $(TARGET_PRODUCT)
 TARGET_PRODUCT_SHORT := $(subst omni_,,$(TARGET_PRODUCT_SHORT))
 
 # Build the final version string
-ifdef BUILDTYPE_RELEASE
-    ROM_VERSION := $(PLATFORM_VERSION)-$(TARGET_PRODUCT_SHORT)
-else
-ifeq ($(ROM_BUILDTIME_LOCAL),y)
-    ROM_VERSION := $(PLATFORM_VERSION)-$(shell date +%Y%m%d-%H%M%z)-$(TARGET_PRODUCT_SHORT)
-else
-    ROM_VERSION := $(PLATFORM_VERSION)-$(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)
-endif
-endif
+
+    ROM_VERSION := $(shell date -u +%Y%m%d)-$(TARGET_PRODUCT_SHORT)
 
 # Apply it to build.prop
 PRODUCT_PROPERTY_OVERRIDES += \
